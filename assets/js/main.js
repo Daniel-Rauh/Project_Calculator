d482ca4644f9f5010e5c8c15bcef19bdfb37bd56
Daniel-Rauh <daniel.rauh@gmail.com>
2020-07-13 09:50:07 +0200

    User no longer allowed zeros

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,24 @@
 let x = ""
 function output() {
-    document.getElementById("display").innerHTML = x
+    if (x.indexOf("0") == 0) {
+        document.getElementById("display").innerHTML = "Please don't start with a 0"
+    } else {
+        document.getElementById("display").innerHTML = x
+    }
 }
 function reset() {
     x = ""
     document.getElementById("display").innerHTML = "<span>.....</span>"
     document.getElementById("last").innerHTML = ""
+    document.getElementById("calc").style.backgroundColor = "#39811D"
+    document.getElementById("calc").style.pointerEvents = "auto"
 }
 function zero() {
     x += "0"
+    if (x.indexOf("0") == 0) {
+        document.getElementById("calc").style.backgroundColor = "#333"
+        document.getElementById("calc").style.pointerEvents = "none"
+    }
 }
 function one() {
     x += "1"
@@ -61,4 +71,7 @@ function bracketTwo() {
 function calculate() {
     document.getElementById("last").innerHTML = x + " ="
     x = Number(eval(x).toFixed(2))
+    document.getElementById("display").innerHTML = x
+    console.log(x)
+    console.log (eval(x).toFixed(2))
 }
